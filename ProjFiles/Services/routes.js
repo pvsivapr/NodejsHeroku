bb4800bf19ec31ac580676121587ca008c71547d
pvsivapr <pvsivapr@gmail.com>
2017-08-02 17:44:55 +0530

    Update routes.js

--- ProjFiles/Services/routes.js
+++ ProjFiles/Services/routes.js
@@ -76,6 +76,20 @@ var appRouter = function(app, conn)
           return res.send(accountMock);
       }
   });
+  app.post("/ServerData", function(req, res) {
+      if(!req.body.user_name || !req.body.user_password) {
+          return res.send({"status": "error", "message": "missing a parameter"});
+      } else {
+        if(req.body.user_name == "PVSivaprasadR" && req.body.user_password == "$Ivaprasad1991" )
+        {
+          return res.send({"status": "success", "message": server.address().address+":"+server.address().port});
+        }
+        else
+        {
+          return res.send({"status": "error", "message": "wrong username / password"});
+        }
+      }
+  });
   app.post("/account", function(req, res) {
       if(!req.body.username || !req.body.password || !req.body.twitter) {
           return res.send({"status": "error", "message": "missing a parameter"});
