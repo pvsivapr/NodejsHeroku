3b9ab6fa142bc9332746a98666a2a7fff363fc4f
Venkata Sivaprasad Reddy Pulagam <pvsivapr@gmail.com>
2020-06-06 13:55:14 +0530

    Adding node js project for practice

--- ProjFiles/Services/routes.js
+++ ProjFiles/Services/routes.js
@@ -97,6 +97,158 @@ var appRouter = function(app, conn, server)
           return res.send(req.body);
       }
   });
+
+
+
+  //For react application practice
+  //INSERT INTO `employee_data`
+  //INSERT INTO `employee_data` (`id`, `first_name`, `last_name`, `age`, `salary`, `company_name`, `description`, `email_id`) VALUES (NULL, '', '', '', '', NULL, NULL, NULL)
+
+  app.get("/Employees", function(req, res) {
+    var sql = "SELECT `id`,`first_name`,`last_name`,`age` from `employee_data`";
+    conn.query(sql, function (err, result)
+    {
+      if (err)
+      {
+        throw err;
+      }
+      else
+      {
+        console.log("Got all results successfully");
+        var data1 = JSON.stringify(result);
+        // console.log("Result :  ", data1);
+        // return data1;
+        respObject = "Got all results successfullyyyyyyy";
+        console.log(respObject);
+        res.send(data1);
+      }
+      // conn.end();
+    });
+  });
+
+  // app.get("/Employee?id=/", function(req, res) {
+  //   var sql = "SELECT `id`,`first_name`,`last_name`,`age` from `employee_data`";
+  //   conn.query(sql, function (err, result)
+  //   {
+  //     if (err)
+  //     {
+  //       throw err;
+  //     }
+  //     else
+  //     {
+  //       console.log("Got all results successfully");
+  //       var data1 = JSON.stringify(result);
+  //       // console.log("Result :  ", data1);
+  //       // return data1;
+  //       respObject = "Got all results successfullyyyyyyy";
+  //       console.log(respObject);
+  //       res.send(data1);
+  //     }
+  //     // conn.end();
+  //   });
+  // });
+
+  app.get('/Employee/:tagId', function(req, res) {
+    //res.send("tagId is set to " + req.params.id);
+    var sql = "SELECT * from employee_data Where id="+req.params.tagId;
+    conn.query(sql, function (err, result)
+    {
+      if (err)
+      {
+        throw err;
+      }
+      else
+      {
+        console.log("Got all results successfully");
+        var data1 = JSON.stringify(result);
+        // console.log("Result :  ", data1);
+        // return data1;
+        respObject = "Got all results successfullyyyyyyy";
+        console.log(respObject);
+        res.send(data1);
+      }
+      // conn.end();
+    });
+  });
+
+  app.patch('/Employee', function(req, res) {
+    //res.send("tagId is set to " + req.query.id);
+    // UPDATE `employee_data` SET `id`=[value-1],`first_name`=[value-2],`last_name`=[value-3],`age`=[value-4],`salary`=[value-5],`company_name`=[value-6],`description`=[value-7],`email_id`=[value-8] WHERE 1
+    var sql = "UPDATE employee_data SET first_name='"+req.body.first_name+"', last_name='"+req.body.last_name+"', age="+req.body.age+", salary="+req.body.salary+", company_name='"+req.body.company_name+"', description='"+req.body.description+"', email_id='"+req.body.email_id+"' WHERE id="+req.query.id;
+    // console.log(sql);console.log(req.body);
+    // res.send(sql);
+    conn.query(sql, function (err, result)
+    {
+      if (err)
+      {
+        throw err;
+      }
+      else
+      {
+        console.log("Got all results successfully");
+        var data1 = JSON.stringify(result);
+        // console.log("Result :  ", data1);
+        // return data1;
+        respObject = "Got all results successfullyyyyyyy";
+        console.log(respObject);
+        res.send(req.body);
+      }
+      conn.end();
+    });
+  });
+
+  app.delete('/Employee', function(req, res) {
+    //res.send("tagId is set to " + req.query.id);
+    // DELETE FROM `employee_data` WHERE 0
+    if(req.query.id != 1)
+    {
+    var sql = "DELETE FROM employee_data WHERE id="+req.query.id;
+    conn.query(sql, function (err, result)
+    {
+      if (err)
+      {
+        throw err;
+      }
+      else
+      {
+        res.send({"status": "failed", "message": "Record deleted Successfully"});
+      }
+      conn.end();
+    });
+  }
+  else
+  {
+    res.send({"status": "failed", "message": "Unable to delete master record"});
+  }
+  });
+
+  app.post("/Employee", function(req, res) {
+    if(!req.body.id || !req.body.first_name || !req.body.last_name || !req.body.age || !req.body.salary)
+    {
+      res.send({"status": "error", "message": "missings a parameter", "user_id": req.body.user_id});
+    }
+    else
+    {
+      //INSERT INTO `employee_data`(`id`, `first_name`, `last_name`, `age`, `salary`, `company_name`, `description`, `email_id`) VALUES ([value-1],[value-2],[value-3],[value-4],[value-5],[value-6],[value-7],[value-8])
+      var sql = "INSERT INTO employee_data (id, first_name, last_name, age, salary, company_name, description, email_id ) VALUES ("+req.body.id+", '"+req.body.first_name+"', '"+req.body.last_name+"', "+req.body.age+", "+req.body.salary+", '"+req.body.company_name+"', '"+req.body.description+"', '"+req.body.email_id+"')";
+      conn.query(sql, function (err, result)
+      {
+        if (err)
+        {
+          res.send({"status": "success", "message": err.message});
+          // throw err;
+        }
+        else
+        {
+          res.send({"status": "success", "message": "One record inserted"});
+          // console.log("1 record inserted");
+        }
+        // conn.end();
+      });
+    }
+  });
+
+
 }
 module.exports = appRouter;
 //http://localhost:3000/account?username=nraboy
